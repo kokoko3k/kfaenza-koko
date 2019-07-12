d80d9ebba5257770c5f3e330c00116b77fb63514
Antonio Orefice <kokoko3k@gmail.com>
2019-07-12 11:31:18 +0200

    Fixed links for 22 size

--- fix_links.sh
+++ fix_links.sh
@@ -6,15 +6,15 @@
 SCRIPTDIR=${0%/*} 
 cd $SCRIPTDIR/actions || exit
 
-size=16
+size=$1
 cd $size
-
 for file in /usr/share/icons/breeze/actions/$size/* ; do 
 	icon=$(basename $file|rev|cut -c 5-|rev)
 	src_icon=../../source-by-size/$size/$icon.png
 	dst_icon=./$icon.png
 	if [ ! -e $dst_icon ] ; then
 		if [ -e $src_icon ] ; then
+			echo ln -s $src_icon $dst_icon
 			ln -s $src_icon $dst_icon
 		fi
 	fi
