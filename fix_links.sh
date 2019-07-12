0731e8e48760e00e65adbb8066d5a6da4ddbc10f
Antonio Orefice <kokoko3k@gmail.com>
2019-07-12 11:29:05 +0200

    script to fix links from source breeze reference, and fixed links with it

---
+++ fix_links.sh
@@ -0,0 +1,21 @@
+#!/bin/bash 
+
+#	per ogni file di breeze in actions/*
+#		in kfaenza-koko, fai un link ln -s sourcebysize/nome actions/nome
+
+SCRIPTDIR=${0%/*} 
+cd $SCRIPTDIR/actions || exit
+
+size=16
+cd $size
+
+for file in /usr/share/icons/breeze/actions/$size/* ; do 
+	icon=$(basename $file|rev|cut -c 5-|rev)
+	src_icon=../../source-by-size/$size/$icon.png
+	dst_icon=./$icon.png
+	if [ ! -e $dst_icon ] ; then
+		if [ -e $src_icon ] ; then
+			ln -s $src_icon $dst_icon
+		fi
+	fi
+done
